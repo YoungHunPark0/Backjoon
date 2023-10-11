5a946d7100eae9915509455150c63a084fe938d1
PARK YOUNG HUN <123914034+YoungHunPark0@users.noreply.github.com>
2023-10-12 08:40:22 +0900

    20231011

---
+++ 2480 주사위 세개/test_algorithm.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class test_algorithm
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.input_textbox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.output_textbox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button1.Location = New System.Drawing.Point(273, 477)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "출 력"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'input_textbox
+        '
+        Me.input_textbox.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.input_textbox.Location = New System.Drawing.Point(3, 32)
+        Me.input_textbox.Multiline = True
+        Me.input_textbox.Name = "input_textbox"
+        Me.input_textbox.Size = New System.Drawing.Size(389, 203)
+        Me.input_textbox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 21)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "입 력 값"
+        '
+        'output_textbox
+        '
+        Me.output_textbox.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.output_textbox.Location = New System.Drawing.Point(4, 268)
+        Me.output_textbox.Multiline = True
+        Me.output_textbox.Name = "output_textbox"
+        Me.output_textbox.Size = New System.Drawing.Size(386, 203)
+        Me.output_textbox.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label2.Location = New System.Drawing.Point(5, 242)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 21)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "출 력 값"
+        '
+        'test_algorithm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(395, 519)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.output_textbox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.input_textbox)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "test_algorithm"
+        Me.Text = "알고리즘"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents input_textbox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents output_textbox As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ 2480 주사위 세개/test_algorithm.vb
@@ -0,0 +1,74 @@
+﻿Public Class test_algorithm
+    '2439'
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inbox As String = input_textbox.Text
+        Dim input() As String = inbox.Split(" ")
+        Dim outputText As String = ""
+        Dim i As Integer
+        Dim j As Integer
+        'Dim k As Integer
+        'Dim array() As String
+        Dim max As Integer = 0
+        Dim counter(6) As Integer
+        Dim countvalue As Integer
+
+        For i = 0 To input.Length - 1
+            countvalue = Integer.Parse(input(i))
+            For j = 0 To input.Length - 1
+                If Integer.Parse(input(j)) = countvalue Then
+                    counter(countvalue) += 1
+                End If
+            Next j
+
+            If counter(countvalue) = 3 Then
+                outputText = 10000 + countvalue * 1000
+                GoTo skip
+            ElseIf counter(countvalue) = 2 Then
+                outputText = 1000 + countvalue * 100
+                GoTo skip
+            End If
+
+            If countvalue > max Then
+                max = countvalue
+            End If
+        Next i
+
+        If outputText = "" Then
+            outputText = max * 100
+        End If
+
+        'array = inbox
+        'i = array(0)
+        'j = array(1)
+        'k = array(2)
+
+        'If i = j And j = k Then
+        '    outputText = (10000 + (i * 1000))
+        'ElseIf i = j And j <> k Then
+        '    outputText = (1000 + (i * 100))
+        'ElseIf i = k And k <> j Then
+        '    outputText = (1000 + (i * 100))
+        'ElseIf j = k And k <> i Then
+        '    outputText = (1000 + (j * 100))
+        'ElseIf i > j And i > k Then
+        '    outputText = (i * 100)
+        'ElseIf j > i And j > k Then
+        '    outputText = (j * 100)
+        'ElseIf k > i And k > j Then
+        '    outputText = (k * 100)
+        'End If
+skip:
+        output_textbox.Text = outputText
+    End Sub
+
+
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles input_textbox.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button1.PerformClick()
+        End If
+    End Sub
+
+
+End Class

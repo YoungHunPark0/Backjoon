9c461d426f7a7a64ab0da0ac8150129c84cbd3f2
PARK YOUNG HUN <123914034+YoungHunPark0@users.noreply.github.com>
2023-10-18 15:58:49 +0900

    20231018
    폴더옮기고 레벨 변경

---
+++ 브론즈2/1009 분산처리/test_algorithm.vb
@@ -0,0 +1,64 @@
+﻿Public Class test_algorithm
+    '1009 분산처리 - 브론즈2'
+    '문제
+
+    '재용이는 최신 컴퓨터 10대를 가지고 있다. 어느 날 재용이는 많은 데이터를 처리해야 될 일이 생겨서 각 컴퓨터에 1번부터 10번까지의 번호를 부여하고, 10대의 컴퓨터가 다음과 같은 방법으로 데이터들을 처리하기로 하였다.
+    '1번 데이터는 1번 컴퓨터, 2번 데이터는 2번 컴퓨터, 3번 데이터는 3번 컴퓨터, ... ,
+    '10번 데이터는 10번 컴퓨터, 11번 데이터는 1번 컴퓨터, 12번 데이터는 2번 컴퓨터, ...
+    '총 데이터의 개수는 항상 ab개의 형태로 주어진다. 재용이는 문득 마지막 데이터가 처리될 컴퓨터의 번호가 궁금해졌다. 이를 수행해주는 프로그램을 작성하라.
+
+    '입력
+    '입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 그 다음 줄부터 각각의 테스트 케이스에 대해 정수 a와 b가 주어진다. (1 ≤ a < 100, 1 ≤ b < 1,000,000)
+
+    '출력
+    '각 테스트 케이스에 대해 마지막 데이터가 처리되는 컴퓨터의 번호를 출력한다.
+
+    '예제 입력  
+    '5
+    '1 6
+    '3 7
+    '6 2
+    '7 100
+    '9 635
+
+    '예제 출력  
+    '1
+    '7
+    '6
+    '1
+    '9
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inbox As String = input_textbox.Text
+        Dim input() As String = inbox.Split(vbCrLf)
+        Dim outputText As String = ""
+        Dim i As Integer
+        Dim j As Integer
+        Dim t As Integer = CInt(input(0))
+        Dim a As Integer
+        Dim b As Integer
+        Dim array() As String
+        Dim pow As Double
+
+        For i = 1 To t
+            array = input(i).Split(CChar(" "))
+            a = CInt(array(0))
+            b = CInt(array(1))
+
+            For j = 1 To b
+                pow = CInt(Math.Pow(a, (b Mod 4)) Mod 10)
+                'num = CStr(pow.ToString.Substring(pow.ToString.Length - 1, 1))
+            Next j
+            If pow = 0 Then
+                pow = 10
+            End If
+            outputText &= pow & vbCrLf
+        Next i
+
+skip:
+        output_textbox.Text = outputText
+    End Sub
+    'substring(index, length) 문자열의 index부분부터 length만큼 추출 b = CInt(array(1).Substring(array(1).Length - 1, 1)) 해봤는데
+    '9일경우는 9 1 두번만 연속되서 가능한데 7은 7 9 3 1 4번 돔 모두가 같지 않으니 못쓴다.
+    'mid를 사용하면 원하는 입력값의 마지막자리를 고정 안됨
+End Class
---
+++ 브론즈4/2740 기찍N/test_algorithm.vb
@@ -0,0 +1,49 @@
+﻿Public Class test_algorithm
+    '2742 기찍N'
+    '문제
+    '동규는 세수를 하다가 정렬이 하고 싶어졌다.
+    '정수 여러 개를 생각한 뒤에, 이를 오름차순으로 정렬하고 싶어졌다.
+    '정수 여러 개가 주어졌을 때, 가장 작은 수, 입력값의 평균값, 가장 큰 수를 출력하는 프로그램을 작성하시오.
+
+    '입력
+    '정수 세 개가 주어진다. 이 수는 1보다 크거나 같고, 1,000,000보다 작거나 같다. 이 수는 모두 다르다.
+
+    '출력
+    '제일 작은 수, 그 다음 수, 제일 큰 수를 차례대로 출력한다.
+
+    '예제 입력 
+    '3 1 2
+    '예제 출력 
+    '1 2 3
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inbox As String = input_textbox.Text
+        Dim input() As String = inbox.Split(" ")
+        Dim outputText As String = ""
+        Dim i As Integer
+        Dim sum As Integer
+        Dim num As Integer
+        Dim min As Integer = input(0)
+        Dim max As Integer = input(0)
+        Dim ave As Double
+
+        For i = 0 To input.Length - 1
+
+            num = CInt(input(i))
+            If num > max Then
+                max = num
+            ElseIf num < min Then
+                min = num
+            End If
+            sum += num
+        Next i
+        ave = sum / input.Length
+
+        outputText = min & " " & ave & " " & max
+
+skip:
+        output_textbox.Text = outputText
+    End Sub
+    'VAL()은 문자를 숫자로 받는 함수. 반대로 STR()는 숫자를 문자로 받는 함수
+
+End Class
